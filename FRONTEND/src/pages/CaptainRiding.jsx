72f37b30bd7025a7402b2bafcc39eebc35b49dd7
m.iqbal29.work@gmail.com <iqbalmubashir654@gmail.com>
2025-10-07 00:25:18 +0530

    Ride Spelling

--- FRONTEND/src/pages/CaptainRiding.jsx
+++ FRONTEND/src/pages/CaptainRiding.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { useRef , useState} from "react";
-import FinishRide from "../components/FinishRIde";
+import FinishRide from "../components/FinishRide";
 
 const CaptainRiding = () => {
     const finishRideRef = useRef(null);
